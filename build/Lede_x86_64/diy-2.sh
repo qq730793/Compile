68f8336d1a0adaab6b6a0568d1af4246efe6eb2c
dawei233 <31338729+dawei233@users.noreply.github.com>
2021-04-16 11:11:47 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -3,13 +3,8 @@ ZZZ="package/lean/default-settings/files/zzz-default-settings"
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 
-sed -i "/uci commit fstab/a\uci commit network" $ZZZ
-sed -i "/uci commit network/i\uci set network.lan.ipaddr='10.0.0.1'" $ZZZ              # IPv4 地址(openwrt后台地址)
-sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ           # IPv4 子网掩码
-sed -i "/uci commit network/i\uci set network.lan.gateway='10.0.0.1'" $ZZZ             # IPv4 网关
-sed -i "/uci commit network/i\uci set network.lan.broadcast='10.0.0.255'" $ZZZ         # IPv4 广播
-sed -i "/uci commit network/i\uci set network.lan.dns='223.5.5.5 119.29.29.29'" $ZZZ   # DNS(多个DNS要用空格分开)
-sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                      # 去掉LAN口使用内置的 IPv6 管理
+# 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile   # 强制选择argon为默认主题选项
 sed -i "/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/argon" $ZZZ        # 选择argon为默认主题
